8792cd58fb2c1a146cbde4faa016ec98732578c2
jiwon yu <yjw5602@naver.com>
2025-01-20 17:52:32 +0900

    filter 1번, 2번 변경

--- mize_intern_pjt/src/components/Modal/FilterModal.jsx
+++ mize_intern_pjt/src/components/Modal/FilterModal.jsx
@@ -30,29 +30,6 @@ export default function FilterModal({
     }, 300);
   };
 
-  // 하위 카테고리 선택 시 실행되는 함수
-  // const handleSubRoomSelect = (subRoom, containSubRooms) => {
-  //   // Filter.jsx로 선택된 카테고리 정보 전달(선택된 방, 선택된 하위 방, 선택된 방에 속한 하위 방들)
-  //   onRoomSelect(selectedRoom, subRoom, containSubRooms);
-  //   setSelectedSubRoom(subRoom);
-  //   handleModalClose(); //  모달 닫기
-  // };
-
-  // // 카테고리 선택
-  // const handleCategoryClick = (category) => {
-  //   setSelectedCategory(category);
-  //   setSelectedRoom(roomInfo[category][0]); // 기본: 첫 번째 방 선택
-  // };
-
-  // // 방 선택
-  // const handleRoomClick = (room) => {
-  //   setSelectedRoom(room);
-  //   onFilterChange(selectedCategory, selectedRoom); // 상태 변경 이벤트 전달 (비동기지만 순서대로 작동)
-  //   onClose(); // 모달 닫기
-  // };
-
-  // 잘 안되면 아마 상태 변경 비동기인듯..그럼 전에 사용하던 방식으로 하자
-
   // 카테고리 선택
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
@@ -78,7 +55,6 @@ export default function FilterModal({
         </Styled.Header>
         <Styled.RoomCategoryContainer>
           <Styled.RoomCategory>
-            {/* 테스트 */}
             {/* 카테고리 선택 */}
             {Object.keys(roomInfo).map((category) => (
               <Styled.RoomName
@@ -98,7 +74,12 @@ export default function FilterModal({
                 $isSelected={selectedRoom === room}
                 onClick={() => handleRoomClick(room)} // 하위 방 선택
               >
-                {room}
+                {/* 좀 하드코딩.. */}
+                {room === "1번"
+                  ? "1번 소회의실"
+                  : room === "2번"
+                  ? "2번 소회의실"
+                  : room}
               </Styled.SubRoomName>
             ))}
           </Styled.SubRoomCategory>
@@ -107,186 +88,3 @@ export default function FilterModal({
     </>
   );
 }
-
-// import React, { useState, useEffect } from "react";
-// import * as Styled from "./FilterModal_style";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faX } from "@fortawesome/free-solid-svg-icons";
-
-// const ROOM_CATEGORIES = {
-//   basic: {
-//     name: "기본",
-//     subCategories: ["기본"],
-//   },
-//   conference: {
-//     name: "회의실",
-//     subCategories: ["대회의실", "1번 소회의실", "2번 소회의실"],
-//   },
-//   conference1: {
-//     name: "임원실",
-//     subCategories: ["대표님", "이사님", "이사님2"],
-//   },
-//   conference2: {
-//     name: "공용 공간",
-//     subCategories: ["수면실", "휴게실", "서버실테스트테스트", "탕비실"],
-//   },
-// };
-
-// // export default function FilterModal({
-// //   onClose,
-// //   onRoomSelect,
-// //   initialSelection,
-// // }) {
-// //   const [isClosing, setIsClosing] = useState(false); // 모달창 슬라이드 애니메이션
-// //   const [selectedRoom, setSelectedRoom] = useState(initialSelection.room); // 현재 선택된 방 카테고리
-// //   const [selectedSubRoom, setSelectedSubRoom] = useState(
-// //     initialSelection.subRoom
-// //   ); // 현재 선택된 하위 방 카테고리(기본값 전달)
-
-// //   console.log("initalSelection: ", initialSelection);
-
-// //   const handleModalClose = () => {
-// //     setIsClosing(true); // 슬라이드 아웃 애니메이션 시작
-
-// //     // 애니메이션이 완료되면 모달 제거(애니메이션 시간과 동일 0.3초초)
-// //     setTimeout(() => {
-// //       onClose();
-// //     }, 300);
-// //   };
-
-// //   // 하위 카테고리 선택 시 실행되는 함수
-// //   const handleSubRoomSelect = (subRoom, containSubRooms) => {
-// //     // Filter.jsx로 선택된 카테고리 정보 전달(선택된 방, 선택된 하위 방, 선택된 방에 속한 하위 방들)
-// //     onRoomSelect(selectedRoom, subRoom, containSubRooms);
-// //     setSelectedSubRoom(subRoom);
-// //     handleModalClose(); //  모달 닫기
-// //   };
-
-// //   return (
-// //     <>
-// //       {/* 모달 밖 영역 */}
-// //       <Styled.BlurContainer onClick={handleModalClose} $isClosing={isClosing} />
-// //       <Styled.Wrapper $isClosing={isClosing}>
-// //         <Styled.Header>
-// //           방 선택
-// //           <FontAwesomeIcon icon={faX} onClick={handleModalClose} />
-// //         </Styled.Header>
-// //         <Styled.RoomCategoryContainer>
-// //           <Styled.RoomCategory>
-// //             {/* Data에 맞게 목록 렌더링 다시 ~ */}
-// //             {Object.entries(ROOM_CATEGORIES).map(([key, category]) => (
-// //               <Styled.RoomName
-// //                 key={key}
-// //                 $isSelected={selectedRoom === key} // 현재 선택된 방인지 확인
-// //                 onClick={() => setSelectedRoom(key)} // 선택된 방 업데이트
-// //               >
-// //                 {category.name}
-// //               </Styled.RoomName>
-// //             ))}
-// //           </Styled.RoomCategory>
-// //           {/* 하위 방 선택 */}
-// //           <Styled.SubRoomCategory>
-// //             {ROOM_CATEGORIES[selectedRoom].subCategories.map((subRoom) => (
-// //               <Styled.SubRoomName
-// //                 key={subRoom}
-// //                 $isSelected={selectedSubRoom === subRoom}
-// //                 onClick={() =>
-// //                   handleSubRoomSelect(
-// //                     subRoom,
-// //                     ROOM_CATEGORIES[selectedRoom].subCategories // 속한 하위 방들
-// //                   )
-// //                 } // 하위 방 선택
-// //               >
-// //                 {subRoom}
-// //               </Styled.SubRoomName>
-// //             ))}
-// //           </Styled.SubRoomCategory>
-// //         </Styled.RoomCategoryContainer>
-// //       </Styled.Wrapper>
-// //     </>
-// //   );
-// // }
-// export default function FilterModal({
-//   data,
-//   selectedRoom,
-//   onFilterChange,
-//   onClose,
-// }) {
-//   const [isClosing, setIsClosing] = useState(false); // 모달창 슬라이드 애니메이션
-//   // const [selectedRoom, setSelectedRoom] = useState(initialSelection.room); // 현재 선택된 방 카테고리
-//   // const [selectedSubRoom, setSelectedSubRoom] = useState(
-//   //   initialSelection.subRoom
-//   // ); // 현재 선택된 하위 방 카테고리(기본값 전달)
-
-//   const handleModalClose = () => {
-//     setIsClosing(true); // 슬라이드 아웃 애니메이션 시작
-
-//     // 애니메이션이 완료되면 모달 제거(애니메이션 시간과 동일 0.3초초)
-//     setTimeout(() => {
-//       onClose();
-//     }, 300);
-//   };
-
-//   // 하위 카테고리 선택 시 실행되는 함수
-//   // const handleSubRoomSelect = (subRoom, containSubRooms) => {
-//   //   // Filter.jsx로 선택된 카테고리 정보 전달(선택된 방, 선택된 하위 방, 선택된 방에 속한 하위 방들)
-//   //   onRoomSelect(selectedRoom, subRoom, containSubRooms);
-//   //   setSelectedSubRoom(subRoom);
-//   //   handleModalClose(); //  모달 닫기
-//   // };
-
-//   // gpt 방 선택 함수
-//   const handleRoomClick = (category) => {
-//     const rooms = data[category]?.subCategories || [];
-//     const firstRoom = rooms[0] || ""; // 첫 번째 하위 방 선택
-//     onFilterChange(category, firstRoom); // 상태 변경 이벤트 전달
-//   };
-
-//   // gpt 하위 방 선택 함수
-//   const handleSubRoomClick = (room) => {
-//     onFilterChange(selectedRoom.category, room); // 하위 방 선택 처리
-//     // 모달 닫기
-//     handleModalClose();
-//   };
-
-//   console.log("선ㅌ택된 방", selectedRoom);
-
-//   return (
-//     <>
-//       {/* 모달 밖 영역 */}
-//       <Styled.BlurContainer onClick={handleModalClose} $isClosing={isClosing} />
-//       <Styled.Wrapper $isClosing={isClosing}>
-//         <Styled.Header>
-//           방 선택
-//           <FontAwesomeIcon icon={faX} onClick={onClose} />
-//         </Styled.Header>
-//         <Styled.RoomCategoryContainer>
-//           <Styled.RoomCategory>
-//             {/* Data에 맞게 목록 렌더링 다시 ~ */}
-//             {Object.entries(data).map(([key, category]) => (
-//               <Styled.RoomName
-//                 key={key}
-//                 $isSelected={selectedRoom.category === key} // 현재 선택된 방인지 확인
-//                 onClick={() => handleRoomClick(key)} // 선택된 방 업데이트
-//               >
-//                 {category.name}
-//               </Styled.RoomName>
-//             ))}
-//           </Styled.RoomCategory>
-//           {/* 하위 방 선택 */}
-//           <Styled.SubRoomCategory>
-//             {selectedRoom.roomList.map((room) => (
-//               <Styled.SubRoomName
-//                 key={room}
-//                 $isSelected={selectedRoom.room === room}
-//                 onClick={() => handleSubRoomClick(room)} // 하위 방 선택
-//               >
-//                 {room}
-//               </Styled.SubRoomName>
-//             ))}
-//           </Styled.SubRoomCategory>
-//         </Styled.RoomCategoryContainer>
-//       </Styled.Wrapper>
-//     </>
-//   );
-// }
--- mize_intern_pjt/src/pages/Homepage/Filter/Filter.jsx
+++ mize_intern_pjt/src/pages/Homepage/Filter/Filter.jsx
@@ -23,7 +23,7 @@ export default function Filter({ selectedFilter, roomInfo, onFilterChange }) {
     onFilterChange(selectedFilter.category, room);
   };
 
-  console.log("Filter 컴포넌트 category", selectedFilter.category);
+  // console.log("Filter 컴포넌트 category", selectedFilter.category);
 
   return (
     <Styled.Wrapper>
@@ -45,7 +45,12 @@ export default function Filter({ selectedFilter, roomInfo, onFilterChange }) {
               onClick={() => handleRoomClick(room)}
             >
               {/* {displayName} */}
-              {room}
+              {/* 좀 하드코딩.. */}
+              {room === "1번"
+                ? "1번 소회의실"
+                : room === "2번"
+                ? "2번 소회의실"
+                : room}
             </Styled.SubRoomItem>
           );
         })}
@@ -69,116 +74,3 @@ export default function Filter({ selectedFilter, roomInfo, onFilterChange }) {
     </Styled.Wrapper>
   );
 }
-
-// import { useState } from "react";
-// import * as Styled from "./Filter_style";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faBars } from "@fortawesome/free-solid-svg-icons";
-// import { PALETTE } from "@/styles/colors";
-// import FilterModal from "../../../components/Modal/FilterModal";
-
-// export default function Filter({ selectedFilter, data, onFilterChange }) {
-//   // 모달 열 수 있는 메뉴바
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   // 선택된 방 상태 관리
-//   // const [selectedFilter, setSelectedRoom] = useState({
-//   //   room: "basic",
-//   //   subRoom: "기본",
-//   //   containSubRooms: ["기본"],
-//   // });
-
-//   // 모달 열리는 함수
-//   const onMenuClick = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   // FilterModal에서 방이 선택됐을 때 실행될 함수
-//   // const handleRoomSelect = (room, subRoom, containSubRooms) => {
-//   //   setSelectedRoom({
-//   //     room,
-//   //     subRoom,
-//   //     containSubRooms,
-//   //   });
-//   //   // setTimeout(() => {
-//   //   //   setIsModalOpen(false); // 선택 완료 후 모달 닫기 (**1초 후 닫을까?)
-//   //   // }, 500);
-//   //   setTimeout(() => {
-//   //     console.log(room, subRoom);
-//   //     console.log(selectedFilter);
-//   //   }, 2000);
-//   // };
-
-//   // 다른 하위 방 클릭시 실행될 함수
-//   // const handleSubRoomClick = (subRoom) => {
-//   //   setSelectedRoom({
-//   //     ...selectedFilter,
-//   //     subRoom,
-//   //   });
-//   //   // 필터 바뀌는 함수 실행
-//   // };
-
-//   // 다른 하위 방 클릭시 실행될 함수(selected 바꾸기)
-//   const handleSubRoomClick = (room) => {
-//     onFilterChange(selectedFilter.roomCategory, room);
-//   };
-
-//   return (
-//     // <Styled.Wrapper>
-//     //   <Styled.SubRoomList>
-//     //     {selectedFilter.containSubRooms &&
-//     //       selectedFilter.containSubRooms.map((subroom) => (
-//     //         <Styled.SubRoomItem
-//     //           key={subroom}
-//     //           $isSelected={selectedFilter.subRoom === subroom} // 선택된 하위 방 표시
-//     //         >
-//     //           {subroom}
-//     //         </Styled.SubRoomItem>
-//     //       ))}
-//     //   </Styled.SubRoomList>
-//     //   <FontAwesomeIcon
-//     //     onClick={onMenuClick}
-//     //     icon={faBars}
-//     //     color={PALETTE.MAIN_BLACK}
-//     //   />
-//     //   {/* FilterModal컴포넌트에 onClose props 전달 */}
-//     //   {isModalOpen && (
-//     //     <FilterModal
-//     //       onClose={() => setIsModalOpen(false)}
-//     //       onRoomSelect={handleRoomSelect}
-//     //       // 선택된 방 modal에 전달
-//     //       initialSelection={selectedFilter}
-//     //     />
-//     //   )}
-//     // </Styled.Wrapper>
-//     <Styled.Wrapper>
-//       <Styled.SubRoomList>
-//         {selectedFilter.roomList &&
-//           selectedFilter.roomList.map((room) => (
-//             <Styled.SubRoomItem
-//               key={room}
-//               $isSelected={selectedFilter.room === room} // 선택된 하위 방 표시
-//               onClick={() => handleSubRoomClick(room)}
-//             >
-//               {room}
-//             </Styled.SubRoomItem>
-//           ))}
-//       </Styled.SubRoomList>
-//       <FontAwesomeIcon
-//         onClick={onMenuClick}
-//         icon={faBars}
-//         color={PALETTE.MAIN_BLACK}
-//         size="sm"
-//       />
-
-//       {/* FilterModal컴포넌트에 onClose props 전달 */}
-//       {isModalOpen && (
-//         <FilterModal
-//           data={data}
-//           selectedFilter={selectedFilter}
-//           onFilterChange={onFilterChange}
-//           onClose={() => setIsModalOpen(false)}
-//         />
-//       )}
-//     </Styled.Wrapper>
-//   );
-// }
